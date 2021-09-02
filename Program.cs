ae8e41fffbfb4cdcf6a9c471bc6532c5b69a6cc7
Felipe Farias <fefarias97@gmail.com>
2021-09-02 15:25:02 -0300

    Resolução exercício OlaUsuario

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tarefa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeUsuario;
+
+            Console.WriteLine("Digite o seu nome: ");
+            nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine($"Olá, {nomeUsuario}!");
+        }
+    }
+}
